8a810a689190cc93f6bb1a37763b058e3e77ff37
mrakashsaha <mr.akashsaha@gmail.com>
2024-12-12 00:02:22 +0600

    login window ui work done, contact us ui bug fixed

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,14 +1,16 @@
 import React, { useContext, useState } from 'react';
 import { AuthContext } from './Provider/AuthProvider';
+import auth from '../assets/auth.jpg';
 import { useForm } from 'react-hook-form';
 import { fetchURL } from '../../fetchURL';
 import { getAdditionalUserInfo } from 'firebase/auth';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { FaGoogle } from 'react-icons/fa';
 
 const Login = () => {
     const location = useLocation();
     const navigate = useNavigate();
-    const {setUser, setLoading, handleLogin, handleLoginWithGoogle } = useContext(AuthContext);
+    const { setUser, setLoading, handleLogin, handleLoginWithGoogle } = useContext(AuthContext);
 
     const [firebaseError, setFirebaseError] = useState(null);
 
@@ -21,9 +23,9 @@ const Login = () => {
             .then((result) => {
                 const user = result.user;
                 setUser(user);
-                console.log (location.state);
+                console.log(location.state);
                 setLoading(false);
-                navigate(location?.state  ? location?.state : "/");
+                navigate(location?.state ? location?.state : "/");
             })
             .catch((error) => {
                 setFirebaseError(error.code);
@@ -62,7 +64,7 @@ const Login = () => {
                         .then(data => {
                             console.log(data)
                             setLoading(false);
-                            
+
                         })
 
                 }
@@ -82,34 +84,41 @@ const Login = () => {
 
 
     return (
-        <div>
-            <h2 className='text-center text-4xl'>Login to your account</h2>
-            <form className="card-body" onSubmit={handleSubmit(onSubmit)}>
-                <div className="form-control">
-                    <label className="label">
-                        <span className="label-text">Email</span>
-                    </label>
-                    <input type='email' className="input input-bordered" placeholder='Enter Your Email' {...register("email", { required: true })} />
-                    {errors.email && <p className='text-red-600'>Email is required</p>}
+        <div className='bg-red-500/20 bg-blend-overlay hero min-h-screen' style={{ "backgroundImage": `url(${auth})` }}>
+            <div className='hero-content flex-col'>
+                <div className='max-w-md bg-base-100'>
+                    <div className='hero-content flex-col'>
+                        <h2 className=' font-bold text-center text-xl'>LOGIN NOW</h2>
+                        <form className="card-body" onSubmit={handleSubmit(onSubmit)}>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input type='email' className="input input-bordered rounded-none" placeholder='Enter Your Email' {...register("email", { required: true })} />
+                                {errors.email && <p className='text-red-600'>Email is required</p>}
+                            </div>
+
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input type='password' className="input input-bordered rounded-none" placeholder='Enter Password' {...register("password", { required: true })} />
+                                {errors.password && <p className='text-red-600'>Password is required</p>}
+                                {firebaseError && <p className='text-red-600'>{firebaseError}</p>}
+                            </div>
+
+                            <label className="label">
+                                <span className='text-sm' >Don't have an account? <Link className='text-blue-700 underline' to={"/register"}>Register Here</Link> </span>
+                            </label>
+
+
+                            <input className='btn rounded-none bg-[#e90101] text-white px-6 py-2 hover:bg-[#e90101] transition' type="submit" value={'Login'} />
+                            <div className='border border-t my-2'></div>
+                            <a onClick={handleGoogleLogin} className='btn rounded-none'> <FaGoogle />                            Continue with Google</a>
+                        </form>
+                    </div>
                 </div>
-
-                <div className="form-control">
-                    <label className="label">
-                        <span className="label-text">Password</span>
-                    </label>
-                    <input type='password' className="input input-bordered" placeholder='Enter Password' {...register("password", { required: true })} />
-                    {errors.password && <p className='text-red-600'>Password is required</p>}
-                    {firebaseError && <p className='text-red-600'>{firebaseError}</p>}
-                </div>
-
-                <label className="label">
-                    <span>Don't have an account? <Link className='text-blue-700 underline' to={"/register"}>Register Here</Link> </span>
-                </label>
-
-
-                <input className='btn' type="submit" value={'Login'} />
-                <a onClick={handleGoogleLogin} className='btn'>Continue with Google</a>
-            </form>
+            </div>
         </div>
     );
 };
